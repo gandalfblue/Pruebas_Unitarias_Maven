3acc16455c3364abe6d9333fc4c5e973e3b855a8
gandalfblue <casanova01_a@hotmail.com>
2022-05-10 18:43:25 -0500

    v0.1.3 Se crean las pruebas unitarias para la clase BasicCalculator

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class BasicCalculatorTest {
 
     private final BasicCalculator basicCalculator = new BasicCalculator();
@@ -32,7 +30,7 @@ public class BasicCalculatorTest {
         Assertions.assertEquals(expectedValue, result);
     }
 
-    @DisplayName("Testing several sums")
+    @DisplayName("Testing several addition")
     @ParameterizedTest(name = "{0} + {1} = {2}")
     @CsvSource({
             "0,    1,   1",
@@ -40,24 +38,92 @@ public class BasicCalculatorTest {
             "49,  51, 100",
             "1,  100, 101"
     })
-    public void severalSums(Long first, Long second, Long expectedResult) {
+    public void severalAddition(Long first, Long second, Long expectedResult) {
         Assertions.assertEquals(expectedResult, basicCalculator.addition(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
 
     @Test
     void subtract() {
+        // Arrange
+        Long number1 = 5L;
+        Long number2 = 1L;
+        Long expectedValue = 4L;
+
+        // Act
+        Long result = basicCalculator.subtract(number1, number2);
+
+        // Assert
+        Assertions.assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several subtract")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "2,    1,   1",
+            "6,    5,   1",
+            "40,  20,  20",
+            "100,  100, 0"
+    })
+    public void severalSubtract(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.subtract(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+
     }
 
     @Test
     void multiply() {
+        // Arrange
+        Long number1 = 5L;
+        Long number2 = 5L;
+        Long expectedValue = 25L;
+
+        // Act
+        Long result = basicCalculator.multiply(number1, number2);
+
+        // Assert
+        Assertions.assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several multiply")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "1,    2,   2",
+            "4,    5,   20",
+            "10,  20,  200",
+            "100,  100, 10000"
+    })
+    public void severalMultiply(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.multiply(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+
     }
 
     @Test
     void division() {
+        // Arrange
+        Long number1 = 10L;
+        Long number2 = 2L;
+        Long expectedValue = 5L;
+
+        // Act
+        Long result = basicCalculator.division(number1, number2);
+
+        // Assert
+        Assertions.assertEquals(expectedValue, result);
     }
 
-    @Test
-    void returnNumberJoined() {
+    @DisplayName("Testing several division")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "4,    2,   2",
+            "100,    5,   20",
+            "100,  10,  10",
+            "100,  100, 1"
+    })
+    public void severalDivision(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.division(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+
     }
 }
